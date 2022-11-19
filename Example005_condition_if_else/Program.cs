64854b764eda0746eda125b2ece59ccf74c21e9f
Natalia <raison-n@mail.ru>
2022-11-19 16:16:33 +0300

    Add Condition If_Else

---
+++ Example005_condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "бабай")
+{
+    Console.Write("УРА! Это же Бабай!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
