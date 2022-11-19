63801c354ec23fe77a24fdbbcd34fa5affa8d55b
Natalia <raison-n@mail.ru>
2022-11-19 20:34:23 +0300

    Add tasks

---
+++ Homework/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+int number = 5;
+
+int numberSecond;
+numberSecond = 8;
+int result = number * number;
+
+Console.WriteLine(result);
+
+int Result = numberSecond * numberSecond;
+Console.WriteLine($"{numberSecond} * {numberSecond} = {Result}");
+
+*/
+
+int number = 15;
+if (number > 10)
+{
+    Console.WriteLine("Число больше 10");
+}
+else
+{
+    Console.WriteLine("Число не больше 10");
+}
+
+
