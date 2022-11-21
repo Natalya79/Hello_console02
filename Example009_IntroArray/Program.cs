ac4f1d0d679e7e03a8e315731176536c506be6d5
Natalia <raison-n@mail.ru>
2022-11-21 19:44:27 +0300

    Задачи 6? 9 11

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,84 @@
+﻿// Массивы и функции
+/*задача 1
+int Max (int number1, int number2, int number3)
+{
+    int result = number1;
+    if (number2 > result) result = number2;
+    if (number3 > result) result = number3;
+    return result;
+}
+
+int[] array = {1, 5, 8};
+
+int max = Max(array[0], array[1], array[2]);
+ Console.WriteLine(max);
+*/
+
+/*задача 2
+int [] array = {1, 12, 31, 4, 15, 16, 17, 18}; // определить новый массив
+int n = array.Length; // определение количества элементов массива
+int  find = 4; //требуется найти элемент массива, равный find
+int index = 0; // установить счётчик index
+while (index < n)
+{
+    if(array[index] == find) // если элемент массива с данным индексом совпал с find, алгоритм завершает работу
+    
+        {
+            Console.WriteLine(index);
+            break; // нужно на случай, если в массиве есть элементы с одинаковым значением.
+            // тогда программаостановится на первом из них 
+        }
+    
+    index = index + 1;
+}
+*/
+
+void FillArray(int[] collection) // FillArray наименование метода. collection наименование аргумента
+{
+    int length = collection.Length; 
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // генератор случайных чисел от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int count = col.Length; 
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while (index < count)
+    {
+        if (collection [index] == find)
+        {
+                position = index;
+                break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int [] array = new int[10]; //создай новый массив с 10 элементами
+
+FillArray(array);
+PrintArray (array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
---
+++ Seminar2/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+void Zadacha1()
+{
+    // Напишите программу, которая выдаёт случайное число из отрезка от 10 до 99 и показывает наибольшую цифру числа
+    Random rand = new Random();//переменная rand, в которую будет помещён генератор случайных чисел new Random
+    int number = rand.Next(10, 100);
+    Console.WriteLine("Случайное число: " + number);
+    int tens = number / 10;
+    int ones = number % 10;
+
+    Console.Write("Наибольшая цифра числа: ");
+    if (tens > ones)
+    {
+        Console.WriteLine(tens);
+    }
+    else
+    {
+        Console.WriteLine(ones);
+    }
+}
+Zadacha1();
+
+*/
+
+/*
+void Zadacha2()
+{
+    // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру числа
+    Random rand = new Random();//переменная rand, в которую будет помещён генератор случайных чисел new Random
+    int number = rand.Next(100, 1000);
+    Console.WriteLine("Случайное число: " + number);
+    int hundreds = number / 100;
+    int ones = number % 10;
+    Console.Write($"{hundreds}{ones}");
+
+}
+Zadacha2();
+*/
+
+
+void Zadacha3()
+{
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    if (number2 % number1 == 0) Console.WriteLine($"{number2} кратно {number1}");
+    else Console.WriteLine($"{number2} не кратно {number1}, остаток {number2 % number1}"); 
+}    
+Zadacha3();
