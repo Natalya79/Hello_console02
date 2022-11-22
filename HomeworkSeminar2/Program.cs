cf5c26062032c38f09f9ee74708291e98ae8a967
Natalia <raison-n@mail.ru>
2022-11-22 16:14:49 +0300

    Задачи 10, 13, 15

---
+++ HomeworkSeminar2/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+
+// Задача 10: напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа
+
+void Zadacha10()
+
+{
+    Console.WriteLine("Введите трёхзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int result = number / 10 % 10;
+    Console.WriteLine(result);
+}
+Zadacha10();
+*/
+
+
+
+/*
+// Задача 13: напишите программу, которая с помощью деления выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет
+
+void Zadacha13()
+
+{
+    Console.WriteLine("Введите  число");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+       while (number > 999)  number = number / 10;
+    
+            if (number / 100 == 0)  
+        {
+            Console.WriteLine("Третьей цифры нет"); 
+        }
+    
+        else  Console.WriteLine($"{number = number % 100 % 10}");  
+    
+   
+}
+Zadacha13();
+*/
+
+
+/*
+// Задача 15: напишите программу, которая принимает на вход цифру, обозначающую день недели, и 
+//проверяет, является ли этот день выходным
+
+
+/* void Zadacha15()
+
+{
+Console.WriteLine("Введите цифру от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1 || number == 2 || number ==3 || number ==4 || number ==5)
+{
+    Console.WriteLine("Рабочий день");
+}
+
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+
+
+else     Console.WriteLine("Некорректная цифра");
+
+}
+Zadacha15();
+
+*/
--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -37,8 +37,11 @@ void Zadacha2()
 Zadacha2();
 */
 
-
+/*
 void Zadacha3()
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли второе кратным первому, 
+// если нет, выводит остаток
 {
     Console.WriteLine("Введите первое число");
     int number1 = Convert.ToInt32(Console.ReadLine());
@@ -48,3 +51,55 @@ void Zadacha3()
     else Console.WriteLine($"{number2} не кратно {number1}, остаток {number2 % number1}"); 
 }    
 Zadacha3();
+
+*/
+
+/*
+// Напишите программу, которая принимает на вход число и проверяет, является ли оно кратным 
+// 7 и 23 одновременно
+Вариант 1
+void Zadacha3()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    if (number % (7 * 23) == 0) Console.WriteLine($"{number} кратно и 7, и 23");
+    else Console.WriteLine($"{number} не кратно 7 и 23 одновременно"); 
+}    
+Zadacha3();
+
+
+//Вариант 2
+void Zadacha3()
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    if (number % (7) == 0 && number % (23) == 0) // & операнд "и" (&& - и краткое, т.е достаточно проверить первое условие),
+    // - если оно не соблюдается, то дальше проверять не нужно,т.к. неверно всё выражение)
+    // |, || операнд "или" (|| - краткое)
+    {
+        Console.WriteLine($"{number} кратно и 7, и 23");
+    }
+    else Console.WriteLine($"{number} не кратно 7 и 23 одновременно"); 
+}    
+Zadacha3();
+*/
+
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно квадратом другого
+
+void Zadacha4()
+{
+   Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    if (number1 * number1 == number2 || number2 * number2 == number1)
+    {
+        Console.WriteLine("Да"); 
+    }    
+    else Console.WriteLine("Нет");
+}
+
+Zadacha4();
