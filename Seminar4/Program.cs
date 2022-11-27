15b04f228d9548c2aa88d5ba3e5d088248cf0289
Natalia <raison-n@mail.ru>
2022-11-27 12:08:15 +0300

    t

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -36,7 +36,7 @@ void Zadacha24()
     Console.WriteLine("Ответ:" + sum);
 }
 Zadacha24();
-8
+
 */
 
 /*
@@ -100,6 +100,8 @@ void Zadacha26()
 }
 Zadacha26();
 */
+
+/*
 //28 Напишите программу, которая на вход принимает число А и выдаёт произведение чисел от 1 до А
 void Zadacha28()
 {
@@ -118,3 +120,100 @@ void Zadacha28()
     Console.WriteLine("Ответ:" + result);
 }
 Zadacha28();
+*/
+
+/*
+// 28 Второй способ
+void Zadacha28()
+{
+    Random rand = new Random();
+    int number = rand.Next(1, 16);
+    Console.WriteLine("Введено число: " + number);
+    int result = 1;
+    while (number > 1)
+    {
+        result *= number;//result = result * counter;
+        number--;
+    }
+    Console.WriteLine("Ответ:" + result);
+}
+Zadacha28();
+*/
+/*
+// 28  способ через for
+void Zadacha28()
+{
+    Random rand = new Random();
+    int number = rand.Next(1, 30);
+    Console.WriteLine("Введено число: " + number);
+    int result = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine($"i = {i}");
+        result = result * i;
+    }
+    Console.WriteLine($"Результат произведения числел от 1 до {number} = {result}");
+}
+Zadacha28();
+*/
+/*
+// 30. Напишите программу, которая выводит массив из 8 элементов, заполнненных 1 и 0 в случайном порядке
+
+void Zadacha30()
+{
+    Random rand = new Random();
+    int[] binar = new int[8];
+    Console.Write("binary array = [ "); // бинарный массив
+
+    for (int i = 0; i < 8; i++)
+    {
+        binar[i] = rand.Next(0, 2);
+        Console.Write(binar[i] + ", ");
+    }
+    Console.WriteLine("]");
+}
+Zadacha30();
+*/
+
+// 30. Напишите программу, которая выводит массив из 8 элементов, заполнненных 1 и 0 в случайном порядке
+//второй способ
+
+void FillArray(int[] nums)
+{
+    Random rand = new Random();
+    int size = nums.Length;
+    for (int i = 0; i < size; i++)
+    {
+        nums[i] = rand.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] nums)
+ 
+{
+    int size = nums.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(nums[i] + "  "); 
+    }
+}
+
+
+void Zadacha30()
+{
+    int size = 8;
+    Random rand = new Random();
+    int[] numbers = new int[size];
+    // у массива после типа данных прописываются [] - т.е. не один int, а набор intов
+    // numbers - это переменная-контейнер для массива. int[] справа - также тип данных, в скобках требуется 
+    // размер. Указывается переменная  size. Слева - создание переменной, где будет храниться массив
+    // справа - создание массива
+    //Console.WriteLine(numbers[7]); // Обращение к массиву. Его размер 7 - это индекс старшего, 8го элемента
+    // пустой массив - это массив из нулей
+
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+    Zadacha30();
+
