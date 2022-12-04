1666a25bc85416733441e0d332e076bdc7890637
Natalia <raison-n@mail.ru>
2022-12-04 18:52:08 +0300

    задачи 34 и 36

--- HomeworkSeminar5/Program.cs
+++ HomeworkSeminar5/Program.cs
@@ -46,7 +46,7 @@
     }
 
 }
-// Zadacha34();
+ Zadacha34();
 
 void Zadacha36()
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов 
@@ -96,55 +96,5 @@ void Zadacha36()
     }
 
 }
-// Zadacha36();
+ Zadacha36();
 
-
-void Zadacha38()
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом 
-// массива
-{
-    int size = 7;
-    int[] numbers = new int[size];
-    FillArray(numbers, 1, 10);
-    PrintArray(numbers);
-    FindResult(numbers);
-
-    void FillArray(int[] nums, int minValue, int maxValue)
-    {
-        Random rand = new Random();
-        int length = nums.Length;
-        for (int i = 0; i < length; i++)
-        {
-            nums[i] = rand.Next(minValue, maxValue);
-        }
-
-    }
-
-    void PrintArray(int[] nums)
-    {
-
-        int length = nums.Length;
-        Console.WriteLine("Вывод массива:");
-        for (int i = 0; i < length; i++)
-        {
-            Console.Write(nums[i] + " ");
-        }
-        Console.WriteLine();
-
-    }
-    void FindResult(int[] nums)
-    {
-        int result = 0;
-        int length = nums.Length;
-        for (int i = 0; i < length; i++)
-        {
-            if (i % 2 != 0)
-            {
-                result = result + nums[i];
-            }
-        }
-        Console.WriteLine("Сумма элементов массива с нечётными индексами:  " + result);
-    }
-
-}
-// Zadacha38();
