8f44055740e939c6bb135dfad6f9b65673d20f92
Natalia <raison-n@mail.ru>
2022-12-18 10:58:41 +0300

    _

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//46 Заполните двумерный массив MxN
+void Zadacha46()
+{
+    int rows = 3;
+    int colums = 4;
+    int[,] numbers = new int[rows, colums];
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+
+void FillArray(int[,] numbers, int minValue = 0, int maxValue = 9)
+{
+    maxValue++;
+    Random rand = new Random();
+    int rows = numbers.GetLength(0);
+    
+    
+    
+}
