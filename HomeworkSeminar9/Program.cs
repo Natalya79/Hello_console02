b3fdcdecced6a6291426929f8efee2604c5de34b
Natalia <raison-n@mail.ru>
2022-12-30 19:56:26 +0300

    ЗАДАЧИ к семинару9

---
+++ HomeworkSeminar9/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите рекурсивный метод, которая найдёт сумму
+натуральных элементов в промежутке от M до N. 
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите начальное число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
